edac340cb66c24f7e8d33331e01a31c186383870
SetaSensei <sensei.seta@gmail.com>
2019-06-25 14:33:40 +0200

    Creation premier code stupid

---
+++ csharp-project/Exercises/Alarm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Answer
+{
+    public class Alarm
+    {
+        public void Check()
+        {
+            if (Clock.GetClock().GetStatus() == 4) Console.WriteLine("Ring ring ring ring ring !");
+        }
+    }
+
+    public class Clock
+    {
+        private Clock()
+        {
+        }
+
+        private static Clock ClockInstance { get; set; }
+
+        public static Clock GetClock()
+        {
+            return ClockInstance ?? (ClockInstance = new Clock());
+        }
+
+        public int GetStatus()
+        {
+            if (DateTime.Now.Millisecond % 5 == 0)
+                return 5;
+            if (DateTime.Now.Millisecond % 4 == 0)
+                return 4;
+            if (DateTime.Now.Millisecond % 3 == 0)
+                return 3;
+
+            return 0;
+        }
+    }
+}
